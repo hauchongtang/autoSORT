5b20807c34117586524c476e760bd813ef63ffb5
Hau Chong <thchong-code@users.noreply.github.com>
2020-06-10 22:45:05 +0800

    edited webInteractor: achieved the function of launching target url, …
    …prompting user input for email and pw, autofill them on login screen

--- webInteracter.py
+++ webInteracter.py
@@ -1,19 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Chrome
+
+# User Prompts for future logging in 
+email_ = str(input("Enter your USERNAME: "))
+password_ = str(input("Enter your PASSWORD: "))
 
 # store base url
 # initialize webdriver exe path
-base_url = "https://www.coursera.org"
-driver = webdriver.chrome(
-    executable_path="D:\Python\AutoSubmission\chromedriver.exe")
+base_url = "https://www.coursera.org/?authMode=login"
+driver = Chrome(executable_path="D:\Python\AutoSubmission\chromedriver.exe")
 # force open in maximised window
-driver.maximise_window()
-driver.implicitly_wait(10) # wait 10s before launch for error handling on laggy devices
-driver.get(base_url) # load in browser window
+driver.maximize_window()
+driver.implicitly_wait(10) # assuming that the login screen will appear after x seconds; x = 10
+driver.get(base_url)  # load in browser window
 # check if correct webpage is loaded, assertionerror wil be raised if mismatch
-assert "coursera" in driver.title
+assert "Coursera" in driver.title
 
-driver.close() # exit browser / terminate driver
+# Find HTML/CSS element to insert username and password
+email = driver.find_element_by_name("email")
+password = driver.find_element_by_name("password")
+login = driver.find_element_by_tag_name("button")
+
+# Execute autofill
+email.send_keys(email_)
+password.send_keys(password_)
+# Execute click
+login.click()
 
 
 
@@ -23,3 +34,6 @@ driver.close() # exit browser / terminate driver
 
 
 
+
+
+driver.close()  # exit browser / terminate driver
